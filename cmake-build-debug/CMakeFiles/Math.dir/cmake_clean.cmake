b72e4988b40eff03ec358a47a3d808a19514311e
Nicksechko <sechko2000@mail.ru>
2019-09-14 14:10:48 +0300

    Add cmake-build-debug

---
+++ cmake-build-debug/CMakeFiles/Math.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Math.dir/main.cpp.o"
+  "CMakeFiles/Math.dir/fraction.cpp.o"
+  "Math.pdb"
+  "Math"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Math.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
